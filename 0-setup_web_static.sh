b0d86119fd4ee8f6c86f4195bf484e7d21fe66df
Favourokerri <favourokerri767@gmail.com>
2023-09-07 12:05:21 +0000

    0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,33 +1,27 @@
 #!/usr/bin/env bash
-#Bash script that sets up your web servers
-#for the deployment of web_static
+# Bash script that sets up web servers for the deployment of web_static
+sudo apt-get update
+sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
 
-if ! command -v nginx &>/dev/null; then
-        sudo apt-get update
-        sudo apt-get install nginx
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
+sudo echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
 
-fi
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-
-#html content to test nginx configuration
-html_content="<!DOCTYPE html>
-<html>
-<head>
-<title>Hello World</title>
-</head>
-<body>
-<h1>Hello, World!</h1>
-</body>
-</html>"
-echo "$html_content" > /data/web_static/releases/test/index.html
-
-# Create the new symbolic link
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-#change ownership to the ubuntu user
 sudo chown -R ubuntu:ubuntu /data/
 
 sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
-sudo nginx -t
+
 sudo service nginx restart
