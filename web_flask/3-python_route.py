5af015853df0a2986fed571767aae4d8e5bc8683
Favourokerri <favourokerri767@gmail.com>
2023-09-22 18:01:18 +0000

    3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -11,21 +11,25 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def Hello_HBNB():
+    """displays Hello HBNB!"""
     return ("Hello HBNB!")
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """displays HBNB"""
     return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def display_c(text):
+    """display “C ” followed by the value of the text variable"""
     return f"C {escape(text.replace('_', ' '))}"
 
 @app.route('/python/', defaults={'text': 'is_cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def display_python(text):
+    """displays “Python ”, followed by the value of the text variable"""
     text = text.replace("_", " ")
     return f"Python {escape(text)}"
 
