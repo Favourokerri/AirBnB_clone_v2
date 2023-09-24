a24e015ba8a4b7eb4041e925372e140454592385
Favourokerri <favourokerri767@gmail.com>
2023-09-24 22:58:23 +0000

    7-dump.sql

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
 """
-This script that starts a Flask web application
+starts a Flask web application
 """
 
 from flask import Flask, render_template
+from models import state
 from models import storage
-from models import *
 app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    """displays states in HTML page sorted alphabetically"""
-    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    """display a HTML page with the states listed in alphabetical order"""
+    states = sorted(list(storage.all(state.State).values()), key=lambda x: x.name)
     return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
-def teardown_session(exception):
+def teardown_db(exception):
     """closes the storage on teardown"""
     storage.close()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=5000)
